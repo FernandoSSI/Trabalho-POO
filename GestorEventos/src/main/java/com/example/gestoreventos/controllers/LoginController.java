5f5c401e63b4b4fad3bc5a1fb951332a0f2ec6f7
FernandoSSI <fernandosi2005@gmail.com>
2024-04-22 14:05:42 -0300

    login view (non-functional)

---
+++ GestorEventos/src/main/java/com/example/gestoreventos/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.gestoreventos.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button cadastroButton;
+
+
+
+}
