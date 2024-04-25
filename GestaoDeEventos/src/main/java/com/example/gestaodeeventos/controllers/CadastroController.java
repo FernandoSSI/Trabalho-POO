8701623134d6a6de202c4540e218aa1a4d0c3a3b
FernandoSSI <fernandosi2005@gmail.com>
2024-04-25 11:24:11 -0300

    tela de cadastro, e ligação com seu controller

---
+++ GestaoDeEventos/src/main/java/com/example/gestaodeeventos/controllers/CadastroController.java
@@ -0,0 +1,11 @@
+package com.example.gestaodeeventos.controllers;
+
+import javafx.fxml.FXML;
+
+public class CadastroController {
+
+    @FXML
+    public void cadastrar(){
+
+    }
+}
--- GestaoDeEventos/src/main/java/com/example/gestaodeeventos/controllers/LoginController.java
+++ GestaoDeEventos/src/main/java/com/example/gestaodeeventos/controllers/LoginController.java
@@ -1,7 +1,16 @@
 package com.example.gestaodeeventos.controllers;
 
+import com.example.gestaodeeventos.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -11,10 +20,22 @@ public class LoginController {
     @FXML
     private Button cadastroButton;
 
-
     @FXML
-    public void abrirTelaCadastro(){
-        System.out.println("Tela abrida");
+    public void abrirTelaCadastro(ActionEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("cadastro.fxml"));
+            Parent root = loader.load();
+
+            CadastroController cadastroController = loader.getController();
+
+
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
